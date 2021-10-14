bf84022cb99263c39e90ef2c8a7ad83206eaad68
Mr. Green <pplswg@gmail.com>
2021-10-14 15:19:07 +0300

    Update Header.svelte
    Update header name

--- src/Header.svelte
+++ src/Header.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <header>
-<h1>🔫💬</h1>
+<h1>👾💬</h1>
   {#if $username}
     <div class="user-bio">
       
@@ -20,6 +20,6 @@
 
     {:else}
 
-      <h3>Gun.js Chat</h3>
+      <h3>Dappl Chat</h3>
   {/if}
 </header>
